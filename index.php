835b7f76544c72e5b432c906cf554c05c64ca16c
Galacor <dtcortezm2@gmail.com>
2023-05-02 22:50:50 -0500

    Guardado

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ document.getElementById("elnombre").innerHTML="Cortez Petter";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/RodriguezGiovanna/'>Rodriguez Giovanna</a></p><p><a href='https://congresoutlvte.org/RamiresMaria/'>Ramires Maria</a></p> <p><a href='https://congresoutlvte.org/OrtizNixon/'>Ortiz Nixon</a></p> ";
  </script>
  
 <?php
