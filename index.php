f181f14f13170b42450a002a7c86ebbf7d6ca714
Galacor <dtcortezm2@gmail.com>
2023-05-02 20:34:45 -0500

    Guardado

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include("marco-right.php");
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
                    <p class="eys-parrafo">Hola, yo soy Petter Kadyr <b> Cortez Mancilla </b>, soy estudiante de la carrea Tecnologias de la Información </p>
-                   <p class="eys-parrafo">Estudio en la Universidad Tecnica Luis Vargas Torres de Esmeraldas </p>
+                   <p class="eys-parrafo">Estudio en la Universidad Tecnica Luis Vargas Torres de Esmeraldas, la materia que mas me gusta dentro de mi especialidad son las bases de datos, me gusta mucho lo que es realizarlas y a la hora de resolver algun problema que estas tengan y rrealizar sus diversas funciones como lo puede ser buscar ciertos registros de algun tipo ya sea por su ID o número de cedula </p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -33,15 +33,15 @@ include("marco-right.php");
     
 
 
-                   <p class="eys-parrafo">En el bachillerato estudie en la Unidad Educativa Fiscomicional María Auxiliadora en la carrera de informatica y donde consegui mi titulo de Técnico de Servicios Administración de Sistemas</p>
-
+                   <p class="eys-parrafo">En el bachillerato estudie en la Unidad Educativa Fiscomicional María Auxiliadora en la carrera de informatica y donde consegui mi titulo de Técnico de Servicios Administración de Sistemas y fue aqui donde me empezo a gustar mas sobre esta carrera y por eso la segui eligiendo para poder aprender mas sobre ella</p>
+                   <p class="eys-parrafo">Soy un apasionado de la tecnología desde la secundaria, quizás sea por esa capacidad que el conocimiento en estos dos campos puede solucionar muchos problemas de la sociedad, es por eso que desde que terminé mi bachillerato técnico me he propuesto seguir con esta carrera sobre las tecnologias del mundo.</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
     
 
 
-                   <p  class="eys-parrafo"> Trabajo ocacionalmente realizando mantenimoento a computadoras y celulares.</p> 
+                   <p  class="eys-parrafo">Mi primer programa lo hice en lenguaje de Java con IDE Netbeans, también otro lenguaje que he usado es el c++ donde lo utilice por primera vez en el bachillerato y realice una calculadora cientifica tambien trabajo ocacionalmente realizando mantenimoento a computadoras y celulares.</p> 
                    
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
